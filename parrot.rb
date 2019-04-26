b61b4c38ecdc321d8f1f490cae9e8cf6207f9f5d
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-04-26 04:44:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (pharse)
+  pharse
+end
+
+
 def parrot (pharse = "Squawk!")
   puts "The parrot will #{pharse} when spoken to"
   pharse
